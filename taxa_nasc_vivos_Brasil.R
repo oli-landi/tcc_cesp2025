7ea057fd5f23a2668dae389f9e1022c418cecac5
Olívia Landi <68713417+oli-landi@users.noreply.github.com>
2025-08-03 15:09:02 -0300

    Add files via upload
    Calcula taxas de nascidos vivos nas faixas 10 a 14, 15 a 19 e 10 a 19 no Brasil em 2022

---
+++ taxa_nasc_vivos_Brasil.R
@@ -0,0 +1,25 @@
+# Pacotes
+library(tidyverse)
+
+# Taxas BR
+
+# Nascidos vivos por 1000 meninas de 10 a 14 anos
+
+taxa <- (14293/6682215) * 1000
+
+# Nascidos vivos por 1000 meninas 15 a 19 anos
+
+taxa2 <- (301313/7058427) * 1000
+
+# Taxa total 10 a 19 anos
+
+pop <- 6682215+7058427
+
+
+taxa_total <- (315606/pop)*1000
+
+# Dados de nascidos vivos obtidos no Tabnet
+# Dados populacionais obtidos pelo Censo 2022 - Panorama do Brasil
+
+
+
